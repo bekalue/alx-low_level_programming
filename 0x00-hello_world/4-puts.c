c914e54dd5bf4e17cc25745ab3f7cfd627467d1d
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-08 06:03:04 -0700

    write c program bruh

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/*
+ * My very first c program as an alx student lol.
+ * this file prints a string.
+ * 2022 GC licenced under bekalu's eyes bruh lol.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"); /*print this string*/
+	return (0);
+}
