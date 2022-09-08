12abca356b8adad2a8d0f5304abd9a5ce917882c
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-08 06:05:38 -0700

    write c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); /*print this string*/
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
