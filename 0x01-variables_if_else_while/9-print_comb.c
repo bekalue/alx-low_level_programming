34fa928b855053d06eb7776f3c825d255a6c92eb
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-09 08:35:57 -0700

    yes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		putchar('\t');
+		putchar(',');
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
