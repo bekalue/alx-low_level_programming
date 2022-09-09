8a35d14b1db4db6cd213c43cd638e8db55470323
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-09 07:50:48 -0700

    yes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int reverse_alphabet = 'z';
+
+	while (reverse_alphabet > 'a')
+	{
+		putchar(reverse_alphabet);
+		reverse_alphabet--;
+	}
+	putchar('\n');
+	return (0);
+}
