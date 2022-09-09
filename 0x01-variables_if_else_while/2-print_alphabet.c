5bb1b0caaa344121fe2223a90afecc711c756668
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-09 05:19:18 -0700

    fix the code

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -16,5 +16,6 @@ int main(void)
 		putchar(alphabets);
 		alphabets++;
 	}
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -25,5 +25,6 @@ int main(void)
 		putchar(alphabets);
 		alphabets++;
 	}
+	putchar('\n');
 	return (0);
 }
