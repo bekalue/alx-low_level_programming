09a2f9d5629c5b213a7cac0b581355ff5880be87
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-09 06:37:32 -0700

    code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers = 0;
+
+	while (numbers < 10)
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	putchar('\n');
+	return (0);
+}
