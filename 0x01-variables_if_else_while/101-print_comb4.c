492c69d1a321ac9903facc1d1021c3849afd378b
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-09 12:41:36 -0700

    yes

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int k;
+
+	n = 48;
+	while (n < 56)
+	{
+		m = 48;
+		while (m < 57)
+		{
+			k = 48;
+			while (k < 58)
+			{
+				if (n < m < k)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(k);
+					if (!(n == 55 && m == 56 && 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			m++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
