94f304c8b6b6b830037c6aeb864e7299af35b4d9
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-14 09:14:24 -0700

    hyjvdafu

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,11 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-/**
- *postive_or_negative - This program will assign a random number to the variable n
- *each time it is executed and prints whether the value is postive or negative.
- *return - success 0.
- */
-void positive_or_negative(int i);
+
+void positive_or_negative(int n);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
