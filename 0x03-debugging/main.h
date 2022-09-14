7da41234dcd54faccd7771fe5d305fb34951ca67
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-14 09:05:05 -0700

    yeah

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ *postive_or_negative - This program will assign a random number to the variable n
+ *each time it is executed and prints whether the value is postive or negative.
+ *return - success 0.
+ */
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
