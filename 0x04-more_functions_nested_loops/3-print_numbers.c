1ddeecd2e39fb3fe76b13b1e1827c47d38945331
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-15 09:23:38 -0700

    hgfthas

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers -  checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char number = '0';
+
+	while (number <= '9')
+	{
+		_putchar(number);
+		number++;
+	}
+}
