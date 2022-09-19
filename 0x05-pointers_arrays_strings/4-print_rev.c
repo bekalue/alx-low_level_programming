33be97764b7ed537fc40b812558d0a432acc3bec
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-19 07:46:12 -0700

    jgyjs

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,24 @@
 #include "main.h"
 
-void print_rev(char *s)
+/**
+* rev_string - Reverses a string
+* @s: Input string
+* Return: String in reverse
+**/
+
+void rev_string(char *s)
 {
-	while (*s != '\0')
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
 	{
-		_putchar(*s--);
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
-	_putchar('\n');
 }
