cac1daac5e07ffa5c6b51043a1b7528375e7e5d0
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-19 23:54:44 -0700

    hfags

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void puts2(char *str)
+{
+	int length = 0, i;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+
+	for (i = 0; i < length; i += 2)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
