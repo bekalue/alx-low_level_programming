0f7ca88f9c8ea2f74023c8658279a9be8b318fd1
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-20 01:18:23 -0700

    hfsdh

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int index = 0, length = 0, n;
+
+	while (str[index++])
+	{
+		length++;
+	}
+	n = (length - 1) / 2;
+
+	if (length % 2 == 0)
+	{
+		for (index = length / 2; index < length; index++)
+			_putchar(str[index]);
+	}
+	else
+	{
+		for (index = n; index < length; index++)
+			_putchar(str[index]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 
 #endif
