43c6cada07444f81f7d94db259589f5339b482d8
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-20 02:16:57 -0700

    hfthas

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,20 @@
 #include "main.h"
-
+/**
+ * print_array - Prints an inputted number of elements
+ * of an array of integers.
+ * @a: The array of integers.
+ * @n: The number of elements to be printed.
+*/
 void print_array(int *a, int n)
 {
 	int index;
 
 	for (index = 0; index < n; index++)
 	{
-		printf("%d", a[index]);
-		if (!(index == (n - 1)))
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		printf("%d", *(a + index));
+		if (index == (n - 1))
+			continue;
+		printf(", ");
 	}
 	_putchar('\n');
 }
