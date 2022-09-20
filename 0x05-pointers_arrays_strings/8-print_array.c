324b8039bc0908e6570b77951a0eebaa5359b068
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-20 01:59:57 -0700

    jhfawh

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,7 +6,7 @@ void print_array(int *a, int n)
 
 	for (index = 0; index < n; index++)
 	{
-		_putchar(*(a + index));
+		printf("%d", *(a + index));
 		if (!(index == (n - 1)))
 		{
 			_putchar(',');
