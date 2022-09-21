971dcdd9d4d97d63acc2842fd4681ed0d5cdac7d
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-21 01:17:43 -0700

    hdsh

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = '\0';
+
+	return (dest);
+}
