8d55828ba22203ef7fc337facd8be6af6d5d008b
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-21 10:02:23 -0700

    please work

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
  * _strcat - function that concatenates
  *          two strings.
  *
- * @dest: pointer to destnation input
+ * @dest: pointer to destination input
  * @src: pointer to source input
  *
  * Return: pointer to resulting string @dest
