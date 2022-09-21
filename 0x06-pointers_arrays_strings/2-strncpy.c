840f8d311fc65f1467685ea95c0178d05b186d91
Bekalu Geta <79188583+bekalue@users.noreply.github.com>
2022-09-21 19:36:48 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,12 @@
 #include "main.h"
-
+/**
+  * _strncpy - Copy a string
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The copy limit
+  *
+  * Return: char value
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index;
