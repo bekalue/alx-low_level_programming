42b70610c4b5f4affb725dbb2db9dd905df875dd
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-22 07:36:39 -0700

    hfsa

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,40 +8,23 @@
  */
 void reverse_array(int *a, int n)
 {
-	int m, k, index = 0, a_len = 0;
+	int m, index = 0, a_len = 0;
 
 	while (a[index] != '\0')
 	{
 		a_len++;
 		index++;
 	}
-	k = (a_len - 1) / 2;
-
-	if (a_len % 2 == 0)
+	
+	for (; index < n && index < a_len / 2 ; index++)
 	{
-		for (; index < n && index < a_len / 2 ; index++)
-		{
-			m = a[index];
-			a[index] = a[a_len - index];
-			a[a_len - index] = m;
-			_printf("%d", a[index]);
-			if (index == (n - 1))
-				continue;
-			printf(", ");
-		}
+		m = a[index];
+		a[index] = a[a_len - 1 - index];
+		a[a_len - 1 - index] = m;
+		_printf("%d", a[index]);
+		if (index == (n - 1))
+			continue;
+		printf(", ");
 	}
-	else
-	{
-		for (; index < n && index < k; index++)
-		{
-			m = a[index];
-                        a[index] = a[a_len - index];
-                        a[a_len - index] = m;
-                        _printf("%d", a[index]);
-                        if (index == (n - 1))
-                                continue;
-                        printf(", ");
-		}
-	}
-	printf("\n");
+	printf("\n")
 }
