0a667f215e931aab3edc5294569271d4d3a3422d
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-22 08:20:57 -0700

    hf

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: s
+ */
 char *string_toupper(char *s)
 {
 	int index = 0;
@@ -7,7 +12,7 @@ char *string_toupper(char *s)
 	while (s[index] != '\0')
 	{
 		if (s[index] >= 'a' && s[index] <= 'z')
-			s[index] += 32;
+			s[index] -= 32;
 	}
 
 	return (s);
