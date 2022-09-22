ff457d9e6f77d038d48ec36b80e2eb36ef520fb9
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-22 08:48:39 -0700

    hsfj

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,31 +9,31 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for(i=0; str[i]!='\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		//check first character is lowercase alphabet
-		if(i==0)
+		/*check first character is lowercase alphabet*/
+		if (i == 0)
 		{
-			if((str[i]>='a' && str[i]<='z'))
-				str[i]=str[i]-32; //subtract 32 to make it capital
-			continue; //continue to the loop
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
+			continue; /*continue to the loop*/
 		}
-		if(str[i]==' ')//check space
+		if (str[i] == ' ')/*check space*/
 		{
-			//if space is found, check next character
+			/*if space is found, check next character*/
 			++i;
-			//check next character is lowercase alphabet
-			if(str[i]>='a' && str[i]<='z')
+			/*check next character is lowercase alphabet*/
+			if (str[i] >= 'a' && str[i] <= 'z')
 			{
-				str[i]=str[i]-32; //subtract 32 to make it capital
-				continue; //continue to the loop
+				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
+				continue; /*continue to the loop*/
 			}
 		}
 		else
 		{
-			//all other uppercase characters should be in lowercase
-			if(str[i]>='A' && str[i]<='Z')
-				str[i]=str[i]+32; //subtract 32 to make it small/lowercase
+			/*all other uppercase characters should be in lowercase*/
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32; /*subtract 32 to make it small/lowercase*/
 		}
 	}
 	return (str);
