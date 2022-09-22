a96532dd1c80cdfa1d26f913ecc0cb017d693b94
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-22 09:01:22 -0700

    hfwsh

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,29 +11,29 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		/*check first character is lowercase alphabet*/
-		if (i == 0)
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
 		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
-			continue; /*continue to the loop*/
-		}
-		if (str[i] == ' ')/*check space*/
-		{
-			/*if space is found, check next character*/
-			++i;
-			/*check next character is lowercase alphabet*/
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
-				continue; /*continue to the loop*/
-			}
-		}
+			if (str[i] >= 'A' && str[i] <= 'Z')
+                                str[i] = str[i] - 32;
+		}`
+
 		else
 		{
-			/*all other uppercase characters should be in lowercase*/
 			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32; /*subtract 32 to make it small/lowercase*/
+				str[i] = str[i] + 32;
 		}
 	}
 	return (str);
