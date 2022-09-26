81e64df64f6456fb90a5e27105aa602779b71b0b
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-26 10:41:22 -0700

    okay

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - function that prints the sum of the two diagonals.
+ * @s: Pointer.
+ * @to: char.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
