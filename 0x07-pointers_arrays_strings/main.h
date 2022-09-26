1aee3edab5a38a940fad5341fee5d0d862f7a879
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-26 08:32:37 -0700

    okay

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <unistd.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
