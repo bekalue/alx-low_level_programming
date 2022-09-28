3d710c7b8064bd46e6bdd5fbcaccac1b193eb20c
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-28 04:15:28 -0700

    okay

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y.
+ * _pow_recursion - a function that returns
+ * the value of x raised to the power of y.
  * @x: a number to be raised by y
  * @y: a power.
  * Return: powerd number.
