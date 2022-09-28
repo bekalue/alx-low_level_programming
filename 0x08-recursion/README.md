a00e0268ec9fa42346595140f4f6008b9a599d65
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-28 02:21:24 -0700

    puts

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,5 @@
+# 0x08-recursion
+
+recursion project
+
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif
