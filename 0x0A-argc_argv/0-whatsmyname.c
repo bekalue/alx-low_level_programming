da74f12c582d925c001c6fe83203b01779d29266
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-30 09:33:49 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: array of argument strings.
  * Return: success (0).
  */
-int main(int argc, char **argv)
+int main(int __attribute__((unused)) argc, char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
