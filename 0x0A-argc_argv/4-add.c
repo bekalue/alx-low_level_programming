a75f91ee237b7fe1a90f8c47cfcaace008723584
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-30 14:20:13 -0700

    so cool

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,32 +24,41 @@ int _atoi(char *s)
 
 	return (num * sign);
 }
+
 /**
- * main - adds multiple numbers
+ * main - adds two positive number
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv: array of arguents
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: 0 (Success), or 1 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0, i;
+	int sum, num, i, j, k;
 
-	if (argc < 2)
-	{
-		printf("%d\n", sum);
-		return (0);
-	}
+	sum = 0;
 
-	for (i = 1; i < argc - 1; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (!(_atoi(argv[i]) >= '0' && _atoi(argv[i]) <= '9'))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
 		}
-		sum += _atoi(argv[i]);
 	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
 	printf("%d\n", sum);
 	return (0);
 }
