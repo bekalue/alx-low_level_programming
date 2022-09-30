b627da2d5473052f47797f2bc9635646d021235d
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-30 12:43:00 -0700

    another task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The integer value of the converted string.
+*/
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc == 3)
+	{
+		num1 = _atoi(argv[1]);
+		num2 = _atoi(argv[2]);
+
+		result = num1 *num2;
+		printf("%d\n", result);
+	}
+	else if(arg < 3 || arg > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
