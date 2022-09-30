35cfd6cdec762908881f87adf906cef37fa34962
Bekalu Endrias <bekalu2054@gmail.com>
2022-09-30 09:47:14 -0700

    task 2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,3 +1,21 @@
 #include "main.h"
-
-
+/**
+ * main - prints number of arguments that are passed when running
+ * the program.
+ * @argc: argument count
+ * @argv: array of argument strings
+ * Return: success (0)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	if (argc > 1)
+	{
+		printf("%d\n", argc);
+	}
+	else
+	{
+		argc = 0;
+		printf("%d\n", argc);
+	}
+	return (0);
+}
