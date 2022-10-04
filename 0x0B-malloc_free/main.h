29b012bb07270a8a93bbd527601a1afba7450f83
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-04 10:33:59 -0700

    yeah so cool

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,4 +7,7 @@
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 #endif
