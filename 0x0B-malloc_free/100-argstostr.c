ea68e0ad26c0e79342e2352eac7a2d55529a6877
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-04 10:41:19 -0700

    ye

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,58 +17,14 @@ char *argstostr(int ac, char **av)
 	}
 	for (n = 0; n < ac; n++)
 	{
-		length += (_strlen(av[n]) + 1);
+		length += (strlen(av[n]) + 1);
 	}
 	str = malloc(length * sizeof(char));
 	str[0] = '\0';
 	for (n = 0; n < ac; n++)
 	{
-		_strcat(str, av[n]);
+		strcat(str, av[n]);
 	}
 	return (str);
 	free(str);
-}
-
-#include "main.h"
-/**
- * _strlen - check the code
- * @s: pointer to a
- * Return: length if success.
- */
-int _strlen(char *s)
-{
-	int length = 0;
-
-	while (*s != '\0')
-	{
-		length++;
-		s++;
-	}
-	return (length);
-}
-
-#include "main.h"
-/**
- * *_strcat - check the code
- * @dest:  Pointers to src
- * @src: pointer to a variable.
- * Return: Retorno
- */
-char *_strcat(char *dest, char *src)
-{
-	int n = 0;
-	int i = 0;
-
-	while (dest[n] != '\0')
-	{
-		n++;
-	}
-	while (src[i] != '\0')
-	{
-		dest[n] = src[i];
-		n++;
-		i++;
-	}
-	dest[n] = '\n';
-	return (dest);
 }
