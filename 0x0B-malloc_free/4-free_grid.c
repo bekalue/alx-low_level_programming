d2503f8c6f409f2f4eb3e539a96502ba8168b605
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-04 07:51:23 -0700

    yeah

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,9 +10,10 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if(grid == NULL || height == 0)
+	if (grid == NULL || height == 0)
 		return;
 
 	for (i = 0; i < height; i++)
 		free(grid[i]);
+	free(grid);
 }
