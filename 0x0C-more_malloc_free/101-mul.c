cc2794363e43c7675b4e3077d0da4f2b2f5e214c
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-06 14:26:38 -0700

    yep

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,61 @@
+#include "main.h"
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The integer value of the converted string.
+*/
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
+/**
+ * main - calculates the multiple of two numbers fro commandline.
+ * @argc: number of argument
+ * @argv: array of argument.
+ * Return: success(0) or exit if it fails.
+ */
+
+int main(unsigned int argc, char *argv[])
+{
+	unsigned int i, j, num1, num2, mul;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error/n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+
+	mul = num1 * num2;
+
+	printf("%d\n", mul);
+	return (0);
+}
