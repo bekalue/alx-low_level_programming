36deb5115a4f78cf1c4df4f2147226ae68604c39
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-10 04:10:07 -0700

    unholy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,10 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
 #endif
