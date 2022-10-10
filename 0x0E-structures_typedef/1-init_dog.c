48dd6c1ffdfbd4b9719bc2eff7469056ab9be95c
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-10 03:01:52 -0700

    intialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - a function that initialize a variable of type struct dog.
+ * @d: object for the struct dog.
+ * @name: a name to be intialized for element <name> of struct dog.
+ * @age: an age to be intialized for an element <age> of struct dog.
+ * @owner: a name of owner to be intialized to an element <owner>
+ * of struct dog.
+ *
+ * Return: nothing.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
