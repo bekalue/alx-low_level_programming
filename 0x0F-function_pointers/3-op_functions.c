fb48e7d6f65d80a5505d86c7ff90c77427fae2fb
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-12 07:48:16 -0700

    okay

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integers.
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: sum.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - difference b/n integers.
+ * @a: an integer.
+ * bb: an integer.
+ *
+ * Return: diference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_sub - multiplication b/n integers.
+ * @a: an integer.
+ * bb: an integer.
+ *
+ * Return: multiplied result.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_sub - division b/n integers.
+ * @a: an integer.
+ * bb: an integer.
+ *
+ * Return: divided result.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulus of integers.
+ * @a: an integer.
+ * bb: an integer.
+ *
+ * Return: reminder.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
