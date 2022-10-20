4a93e390999c4416a04a10ac27081c38b72ec66e
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-20 14:06:40 +0300

    add

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,20 @@
-#include "main.h"
+#include "lists.h"
 
 size_t print_list(const list_t *h)
 {
+	size_t n = 0;
 
+	if (!h)
+		return(n);
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
