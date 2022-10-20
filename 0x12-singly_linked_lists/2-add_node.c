0b5aace8722ca31a98e1fdc4ace7e63eb9f41e11
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-20 16:22:28 +0300

    yeah

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * @brief 
+ * 
+ * @param head 
+ * @param str 
+ * @return list_t* 
+ */
+list_t *add_node(list_t **head, const char *str)
+{;
+	list_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->len = strlen(str);
+	new->str = strdup(str);
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
