66db88146f3effa50a2ee27c166a8c686ac4f224
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-20 20:49:33 +0300

    shi

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-global _start
-    extern printf
-    section .text
-_start:
-    mov     rdi, format
-    mov     rsi, message
-    mov     rax, 0
-    call    printf
-    mov rax, 1
-    mov rbx, 0
-    int 80h
-    section .data
-message: db      'Hello, Holberton',0x0a,0
-format:    db '%s', 0
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
