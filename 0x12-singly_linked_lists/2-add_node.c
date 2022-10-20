679a7e80d2c5024eaf81c8cb03273adc16262711
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-20 16:26:43 +0300

    oka

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,10 +1,12 @@
 #include "lists.h"
+#include <string.h>
 /**
- * @brief 
- * 
- * @param head 
- * @param str 
- * @return list_t* 
+ * add_node - adds a new node at the beginning of a
+ * \ list_t list.
+ * @head: a pointer to a pointer.
+ * @str: a string.
+ *
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
