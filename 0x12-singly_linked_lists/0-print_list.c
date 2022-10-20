d0f6b6cba6fd405f71409aa7b57b44d7a60d2fe0
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-20 14:09:59 +0300

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
+/**
+ * print_list - prints linked lists from head to tail.
+ * @h: head of linked list.
+ *
+ * Return: number of linked list or members of list
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
 	if (!h)
-		return(n);
+		return (n);
 
 	while (h)
 	{
