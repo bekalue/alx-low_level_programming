bac63627a61f023cf571cb9be83b2ca7e76e2c13
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-20 14:34:41 +0300

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * list_len - a function that returns the number of elements in a linked list_t list.
+ * @h: head node.
+ *
+ * Return: number of elements
+ */
 size_t list_len(const list_t *h)
 {
 	size_t num = 0;
