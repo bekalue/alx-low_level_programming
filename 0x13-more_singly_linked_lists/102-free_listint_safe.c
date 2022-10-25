5c7692975b376cb51b625ff906d69dd38da5227c
Bekalu Endrias <bekalu2054@gmail.com>
2022-10-25 11:45:22 +0300

    cool

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* free_listint_safe - frees a linked list safely
+* @h: holds a pointer to a pointer of a linked list
+* Return: the size of the list
+*/
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *slow_ptr, *fast_ptr, *free_ptr;
+	size_t size;
+
+	size = 0;
+
+	if (!h || *h == NULL)
+		return (0);
+	fast_ptr = (*h)->next;
+	slow_ptr = *h;
+
+	while (fast_ptr && fast_ptr < slow_ptr)
+	{
+		free_ptr = slow_ptr;
+		fast_ptr = fast_ptr->next;
+		slow_ptr = slow_ptr->next;
+		size += 1;
+
+		free(free_ptr);
+	}
+	size += 1;
+	free(slow_ptr);
+	*h = NULL;
+	return (size);
+}
