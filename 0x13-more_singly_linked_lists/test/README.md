c21f96b09421df543a9a8ce1e03535ff997b1383
Bekalu Geta <bekalu2054@gmail.com>
2022-10-26 16:06:35 +0300

    cool

---
+++ 0x13-more_singly_linked_lists/test/README.md
@@ -0,0 +1,25 @@
+# File main_5.c 
+
+it was a test file for [101-print_listint_safe.c](https://github.com/bekalue/alx-low_level_programming/blob/master/0x13-more_singly_linked_lists/101-print_listint_safe.c) but showed error.
+
+output:
+
+```
+1
+4
+-> 3
+2
+```
+
+Expected:
+
+```
+1
+4
+3
+2
+5
+-> 3
+5
+```
+
---
+++ 0x13-more_singly_linked_lists/test/main_5.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t _free_listint_safe(listint_t **);
+
+/**
+ * _add_node - Add a node at the beginning of a list
+ *
+ * @head: A pointer to the first element of a list
+ * @n: The number to store in the new node
+ *
+ * Return: The address of the new node
+ */
+listint_t *_add_node(listint_t **head, int n)
+{
+	listint_t *tmp;
+
+	tmp = malloc(sizeof(*tmp));
+	if (!tmp)
+		return (NULL);
+	tmp->n = n;
+	tmp->next = *head;
+	*head = tmp;
+	return (tmp);
+}
+
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *node1;
+	listint_t *node2;
+	listint_t *node3;
+	listint_t *node4;
+	listint_t *node5;
+	size_t n;
+
+	head = NULL;
+    /* 1 -> 4 -> 3 -> 2 -> 5 -> (3) */
+	node5 = _add_node(&head, 5);
+	node4 = _add_node(&head, 4);
+	node3 = _add_node(&head, 3);
+	node2 = _add_node(&head, 2);
+	node1 = _add_node(&head, 1);
+
+	node1->next = node4;
+	node4->next = node3;
+	node3->next = node2;
+	node2->next = node5;
+	node5->next = node3;
+
+	n = print_listint_safe(head);
+	printf("%lu\n", n);
+	_free_listint_safe(&head);
+	return (0);
+}
