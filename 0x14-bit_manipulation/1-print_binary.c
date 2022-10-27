e2986aea4b58e8b38769dad68f1ca5d505facbe0
Bekalu Geta <bekalu2054@gmail.com>
2022-10-27 15:18:00 +0300

    cool

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_binary - prints a binary representation of a number
+* @n: holds the number to change
+*/
+
+void print_binary(unsigned long int n)
+{
+	if ((n >> 1) == 0 && n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n >> 1 != 0)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+
+}
