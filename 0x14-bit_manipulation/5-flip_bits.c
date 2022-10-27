e0c3cf95acd3f8eb4d54e9e76baecac9bc493151
Bekalu Geta <bekalu2054@gmail.com>
2022-10-27 16:38:25 +0300

    okay

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* flip_bits - returns the number of bits you would need to flip to get fmor one
+* number to another
+* @n: the number
+* @m: the 2nd number
+* Return: the count
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int count, difference;
+
+	difference = n ^ m;
+	count = 0;
+	for (count = 0; difference != 0; count += 1)
+		difference &= (difference - 1);
+	return (count);
+}
