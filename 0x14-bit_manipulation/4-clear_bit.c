c07e6d2da17039ff6a161b0f5b685487a8ce8699
Bekalu Geta <bekalu2054@gmail.com>
2022-10-27 16:35:32 +0300

    cool

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* clear_bit - clears the bit at a given index
+* @n: a pointer to the number
+* @index: the index the change
+* Return: success or -1
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
