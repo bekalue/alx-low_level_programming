da3a56b2c7e00d2204592ace937282d44277f384
Bekalu Geta <bekalu2054@gmail.com>
2022-10-29 11:50:40 +0300

    commiit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* clear_bit - clears the bit at a given index
+* clear_bit - clears the bit at a given index which means
+* \ it sets a bit to 0 at a given index.
 * @n: a pointer to the number
 * @index: the index the change
 * Return: success or -1
