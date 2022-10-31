6137c51bb4bd2c5029f111d1b1da6c5b75880b0b
Bekalu Geta <bekalu2054@gmail.com>
2022-10-31 20:16:44 +0300

    fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,4 +16,16 @@ int append_text_to_file(const char *filename, char *text_content);
 void err_98(int fd, char *buf, char *argv);
 void err_99(int fd, char *buf, char *argv);
 void err_100(int fd, char *buf);
+char is_elf_file(int fd, void **header);
+void print_elf_header(void *header);
+void print_section(int id, void *header);
+void print_magic(void *header);
+void print_class(void *header);
+void print_data(void *header);
+void print_version(void *header);
+void print_os_abi(void *header);
+void print_abi_version(void *header);
+void print_type(void *header);
+void print_entry_pt_addr(void *header);
+void close_fd(int fd);
 #endif
