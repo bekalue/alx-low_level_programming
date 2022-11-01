c710d3c714ac14c6f0e07e77c7cd2c2e51963672
Bekalu Geta <bekalu2054@gmail.com>
2022-11-01 11:51:21 +0300

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -46,6 +46,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		return (1);
 
 	res = write(fd, text_content, strlen(text_content));
 	if (res == -1)
