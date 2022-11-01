2ad8545a8fb9dc45a35a54fccf2bf344df7abef4
Bekalu Geta <bekalu2054@gmail.com>
2022-11-01 11:54:51 +0300

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,35 +6,7 @@
 * @text_content: a NULL terminated string to write to the file
 * Return: 1 (Success),  -1 (failure)
 */
-/*
-int create_file(const char *filename, char *text_content)
-{
-	int fd;
-	int len, res;
 
-	if (filename == NULL)
-		return (-1);
-	 open
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-	if (fd == -1)
-		return (-1);
-
-	if (!text_content)
-		return (1);
-	len = 0;
-	while (text_content[len] != '\0')
-		len += 1;
-
-	res = write(fd, text_content, len);
-	if (res != len)
-		return (-1);
-	res = close(fd);
-	if (res == -1)
-		exit(-1);
-
-	return (1);
-}*/
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -43,9 +15,10 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	/*open the file*/
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
 	if (fd == -1)
 		return (-1);
+	/*return if text_content is null*/
 	if (!text_content)
 		return (1);
 
