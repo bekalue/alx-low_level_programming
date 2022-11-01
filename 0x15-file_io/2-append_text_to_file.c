be1022cc4d245caef549cc62f027372555045164
Bekalu Geta <bekalu2054@gmail.com>
2022-11-01 12:41:53 +0300

    common

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
 * @text_content: the text that will be appended to the file
 * Return: 1 (Success), -1 (Failure)
 */
-
+/*
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, res, len;
@@ -30,5 +30,34 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (res == -1)
 		return (-1);
 
+	return (1);
+}*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len, res;
+
+	if (!filename)
+		return (-1);
+	/*open the file*/
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		len = 0;
+		while (text_content[len])
+			len++;
+		res = write(fd, text_content, len);
+		if (res == -1)
+			return (-1);
+
+		res = close(fd);
+		if (res != 0)
+			exit(-1);
+		return (1);
+	}
+	res = close(fd);
+	if (res != 0)
+		exit(-1);
 	return (1);
 }
