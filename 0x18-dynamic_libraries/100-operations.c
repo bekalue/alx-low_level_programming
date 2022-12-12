1e231f998ed757fdb57a54da209885b50f3040fc
Bekalu Geta <bekalu2054@gmail.com>
2022-12-12 13:02:05 +0300

    script fix

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+/**
+ * add - adds two passed integer values.
+ * @x: an integer.
+ * @y: an integer.
+ *
+ * Return: the sum.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+/**
+ * sub - substracts two passed integer values.
+ * @x: an integer.
+ * @y: an integer.
+ *
+ * Return: the diff
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+ * mul - multiplies two passed integer values.
+ * @x: an integer
+ * @y: an integer
+ *
+ * Return: result
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+ * div - divides the two passed integer values
+ * @x: an integer
+ * @y: an integer
+ *
+ * Return: the result
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+/**
+ * mod - computes modulo
+ * @x: an integer
+ * @y: an integer
+ *
+ * Return: the modulus
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
