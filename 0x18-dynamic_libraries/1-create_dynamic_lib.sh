b17df07ed8e20a88393bfc940f7d451bda5324ce
Bekalu Geta <bekalu2054@gmail.com>
2022-12-12 12:21:54 +0300

    script fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -c -fPIC -Werror -Wall -Wextra -pedantic -std=gnu89
+gcc -c -fPIC -Werror -Wall -Wextra -pedantic -std=gnu89 *.c
 gcc -shared *.o -o liball.so
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
