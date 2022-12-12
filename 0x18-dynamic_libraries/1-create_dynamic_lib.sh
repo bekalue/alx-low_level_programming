463491400ce48cb73d1f99a99c274ca87fe3f333
Bekalu Geta <bekalu2054@gmail.com>
2022-12-13 00:11:14 +0300

    i have no idea

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -Wall -Wexta -Werror -pedantic -std=gnu89 -fPIC -c *.c 
-gcc -shared -o liball.so
+gcc -Wall -Wexta -Werror -pedantic -std=gnu89 -fPIC -c *.c
+gcc -shared -o liball.so *.o
