1409ff23770f624d575dd1af36539de9b6d69b2f
Bekalu Geta <bekalu2054@gmail.com>
2023-04-11 11:59:12 +0300

    🔧Fix edge cases

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,6 +18,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	if (array)
 	{
+		if (array[low] == value)
+			return (low);
 		while (low <= high)
 		{
 			pos = low +
@@ -33,7 +35,7 @@ int interpolation_search(int *array, size_t size, int value)
 				low = pos + 1;
 			else if (array[pos] > value)
 				high = pos - 1;
-			else
+			else if (array[pos] == value)
 				return (pos);
 		}
 	}
